5cc32d774149e5887dc6ccf0d7f92207f016c557
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-08 02:20:40 +0700

    Update Info.java
    Added info about the Moon

--- Laba3/src/Info.java
+++ Laba3/src/Info.java
@@ -1,8 +1,14 @@
 public final class Info{
-    private final String info =  "Планета Земля: Большая, имеет 6 континентов и" +
+    private final String infoAboutTheEarth =  "Планета Земля: Большая, имеет 6 континентов и" +
             "4 океана, воздух чистый, состоит преимущественно" +
             "из ислорода и азота, твердой оболочки планета не имеет, населена людьми";
+    private final String infoAboutTheMoon =  "Планета Луна: Маленькая, имеет 1 континент" +
+            "1 океан, воздух грязный, состоит преимущественно" +
+            "из сероводорода и азота, твердой оболочка имеется, населена лунатиками";
     public String getInfoAboutTheEarth() {
-        return this.info;
+        return this.infoAboutTheEarth;
+    }
+    public String getInfoAboutTheMoon() {
+        return this.infoAboutTheMoon;
     }
 }
