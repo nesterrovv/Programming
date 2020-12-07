341d7dbac3354645c4a74fac305a58a0c92ddaa0
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-08 02:21:06 +0700

    Update Journalist.java
    Dialog has been upgraded.
    (method say)

--- Laba3/src/Journalist.java
+++ Laba3/src/Journalist.java
@@ -1,24 +1,33 @@
 public class Journalist {
+
     Info information = new Info(); // Instantiating a class
     Journalist zvezdochka = new Journalist();
     String knowledge = null;
+
     public String say(String name) {
         Journalist zvezdochka = new Journalist();
         System.out.println("Здравствуйте " + name + "!");
         System.out.println("Нам было бы очень интересно услышать о вашей планете...");
-        zvezdochka.setInformation(inform); // Journalist receives the information
+        zvezdochka.setInformationAboutTheEarth(aboutTheEarth); // Journalist receives the information
         if (knowledge != null) {
-            return "Информация получена!";
+            System.out.println("Ого, а наша планета совсем другая!");
+            zvezdochka.getInformationAboutTheMoon(); // To say about the Moon
+            return "Интервью прошло успешно!";
         }
         else {
             return "Журналист не понял интерьвера!";
         }
     }
-    String inform = information.getInfoAboutTheEarth(); // Information
+    String aboutTheEarth = information.getInfoAboutTheEarth(); // Information
+    String aboutTheMoon = information.getInfoAboutTheMoon();
 
-    public void setInformation(String information) { // To receive the information
-        knowledge = information;
+    public void setInformationAboutTheEarth(String inform) { // To receive the information
+        knowledge = inform;
     }
+    public String getInformationAboutTheMoon() { // To take info about herself
+        return aboutTheMoon;
+    }
+    
     /*
     + say(String): String
     + setInformatin(Info)
