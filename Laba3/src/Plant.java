cf559e88dfcd2284c449f7f6f7707a5b25560e89
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-07 22:07:16 +0700

    Create Plant.java

---
+++ Laba3/src/Plant.java
@@ -0,0 +1,13 @@
+public class Plant {
+    private String type;
+    private String homePlanet;
+    private boolean gowingStatus;
+    private boolean inSoil;
+    private boolean isWatered;
+
+    public void grow() {
+        System.out.print("Cложившиеся благоприятные условия помогают ");
+        System.out.println("растению расти. ");
+        System.out.println("Оно растет! Нужно подождать.");
+    };
+}
