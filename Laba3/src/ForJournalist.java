d5e67eb9fdd7d4085647039c128c1696e97eae8b
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-08 13:59:30 +0700

    Create ForJournalist.java

---
+++ Laba3/src/ForJournalist.java
@@ -0,0 +1,16 @@
+public interface ForJournalist {
+    String say(String name);
+    void setInformationAboutTheEarth(String inform);
+    String getInformationAboutTheMoon();
+    boolean isHasSeed();
+    void setTheme(String theme);
+    String getTheme();
+    void setOpponents(String opponent);
+    String getOpponent();
+    void setName(String name);
+    String getName();
+    void setHomePlanet(String homePlanet);
+    String getHomePlanet();
+    void setJob(String job);
+    String getJob();
+}
