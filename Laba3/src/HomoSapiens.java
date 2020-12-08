317d4acd7530b13c75720b1f58ab19ec29b58c2f
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-09 01:53:47 +0700

    Update HomoSapiens.java

--- Laba3/src/HomoSapiens.java
+++ Laba3/src/HomoSapiens.java
@@ -1,20 +1,24 @@
 public abstract class HomoSapiens {
-    private String name;
-    private String homePlanet;
-    private String job;
 
-    public abstract String say(String name);
-    public abstract String eat(Food f);                     // FOR TRAVELLER
+    protected String name;
+    protected String homePlanet;
+    protected String job;
+    protected boolean hasSeed;
+
+    //public abstract String say(String name);
+    //public abstract String eat(Food f);                     // FOR TRAVELLER
         //System.out.println("Путешественник поел" + f);
         //return "Трапеза прошла успешно";
-    public abstract String drink(Drink d);                  // FOR TRAVELLER
+    //public abstract String drink(Drink d);                  // FOR TRAVELLER
         //System.out.println("Путешественник поел" + d);
         //return "Питье прошло успешно";
-    public abstract String health();                        // FOR TRAVELLER
+    //public abstract String health();                        // FOR TRAVELLER
         //Return sth from enum State;
-    public abstract String explore(Info i);                 // FOR SCIENTIST
-    public abstract String explorePlant(Plant p);           // FOR SCIENTIST
+    //public abstract String explore(Info i);                 // FOR SCIENTIST
+    //public abstract String explorePlant(Plant p);           // FOR SCIENTIST
     public abstract void setName(String name);              // FOR ALL
     public abstract String getName();                       // FOR ALL
     public abstract boolean hasSeed();                      // FOR ALL
+    public abstract void setJob(String job);
+    public abstract String getJob();
 }
