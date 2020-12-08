b4ac1b32cb04529497644460215eda0a2233daa7
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-08 17:17:17 +0700

    Create ForScientist.java

---
+++ Laba3/src/ForScientist.java
@@ -0,0 +1,16 @@
+public interface ForScientist {
+    /*
+    public String say(String s);
+    setName(String)
+    getName(): String
+    setHomePlanet(String)
+    getHomePlanet(): String
+    setJob(String)
+    getJob(): String
+    getInformation(): Info
+    isHasSeed()
+    explore(Info): String
+    explorePlant(Plant): String
+
+     */
+}
