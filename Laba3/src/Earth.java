de718ba81f835f622843234eda60643477b3b5b5
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-09 17:45:41 +0700

    Create Earth.java

---
+++ Laba3/src/Earth.java
@@ -0,0 +1,5 @@
+public class Earth extends Planet {
+    private String name = "Неизвестная планета";
+    private String size = "Неизвестный размер";
+    private String type = "Неизвестный тип";
+}
