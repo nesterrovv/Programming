1fa00ceaff6732b86b9351dcc48dd5ba64618bd3
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-09 14:04:32 +0700

    Create Traveler.java

---
+++ Laba3/src/Traveler.java
@@ -0,0 +1,30 @@
+public class Traveler extends HomoSapiens {
+
+
+
+
+    @Override
+    public boolean hasSeed() {
+        return hasSeed;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public String getJob() {
+        return this.job;
+    }
+}
