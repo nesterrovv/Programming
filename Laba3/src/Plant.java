89374c7f3499125bc17647eb474f5688ef748a0f
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-09 13:03:16 +0700

    Update Plant.java

--- Laba3/src/Plant.java
+++ Laba3/src/Plant.java
@@ -1,13 +1,13 @@
 public class Plant {
     private String type;
     private String homePlanet;
-    private boolean gowingStatus;
+    private boolean growingStatus;
     private boolean inSoil;
     private boolean isWatered;
 
     public void grow() {
         if (inSoil == true && isWatered == true) {
-            System.out.print("Cложившиеся благоприятные условия помогают ");
+            System.out.print("Сложившиеся благоприятные условия помогают ");
             System.out.println("растению расти. ");
             System.out.println("Оно растет! Нужно подождать.");
 
