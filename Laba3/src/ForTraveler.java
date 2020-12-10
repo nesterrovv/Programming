0fadac4ec6f341a54b69c0785ca78668117c30fd
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-10 12:21:24 +0700

    Create ForTraveler.java

---
+++ Laba3/src/ForTraveler.java
@@ -0,0 +1,7 @@
+public interface ForTraveler {
+    void eat(Food f);
+    void drink(Drink d);
+    String setHealthStatus();
+    boolean possibilityToGiveSetInformation();
+    boolean returnPossibility();
+}
