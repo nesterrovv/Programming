db8fb4e81512096f64b7c2f8280898cb4af2294c
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-11 15:51:23 +0700

    Update Main.java

--- Laba3/src/Main.java
+++ Laba3/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Все работает");
-        Earth earth = new Earth();
+          System.out.println("");
     }
 }
