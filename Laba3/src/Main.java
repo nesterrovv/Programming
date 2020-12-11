6e88d818b54ff06bc412f306b565d8c1119baa5f
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-11 17:55:17 +0700

    Update Main.java

--- Laba3/src/Main.java
+++ Laba3/src/Main.java
@@ -1,6 +1,51 @@
 public class Main {
 
     public static void main(String[] args) {
-          System.out.println("");
+
+        Traveler traveler = new Traveler();
+        Journalist journalist = new Journalist();
+        Scientist scientist = new Scientist();
+        Info info = new Info();
+        Food food = new Food();
+        Drink drink = new Drink();
+        Seed seed = new Seed();
+        //Location location = new Location();
+        Flight flight = new Flight();
+
+        traveler.setName("Незнайка");
+        traveler.setJob("Путешественник");
+        traveler.setJob("Путешественник");
+
+        traveler.setName("Незнайка");
+        traveler.setJob("Путешественник");
+        traveler.setJob("Путешественник");
+
+        traveler.setName("Незнайка");
+        traveler.setJob("Путешественник");
+        traveler.setJob("Путешественник");
+
+        traveler.takeSeed(); // To give the seeds
+
+        flight.setStartPosition(Location.EARTH);
+        flight.setFinishPosition(Location.MOON);
+        flight.start();
+
+        if (flight.where() == Location.SPACE) {
+            while (traveler.getHealth() != true) {
+                traveler.getHealthStatus();
+                traveler.eat(food);
+                traveler.getHealthStatus();
+                traveler.drink(drink);
+            }
+        }
+
+        flight.changeLocation(Location.MOON);
+
+        if (flight.where() == Location.MOON) {
+            //journalist.say
+        }
+
+
+
     }
 }
