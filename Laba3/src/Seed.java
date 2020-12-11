b297fa950b8444774c06bf1bb6f50699bd3a6aa0
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-12 02:19:56 +0700

    Update Seed.java

--- Laba3/src/Seed.java
+++ Laba3/src/Seed.java
@@ -2,4 +2,7 @@ public class Seed extends Plant {
     public String isPlant() {
         return "Семена еще не проросли! Подождите. ";
     }
+    public String dna() {
+        return "AATCTTATGATATCAATCG";
+    }
 }
