180551b8f43c452d28439f26d5f3188d9afe1069
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-11 17:55:13 +0700

    Update Info.java

--- Laba3/src/Info.java
+++ Laba3/src/Info.java
@@ -1,18 +1,17 @@
 public final class Info{
-    private final String infoAboutTheEarth =  "Планета Земля: Большая, имеет 6 континентов и" +
-            "4 океана, воздух чистый, состоит преимущественно" +
-            "из ислорода и азота, твердой оболочки планета не имеет, населена людьми";
-    private final String infoAboutTheMoon =  "Планета Луна: Маленькая, имеет 1 континент" +
-            "1 океан, воздух грязный, состоит преимущественно" +
-            "из сероводорода и азота, твердой оболочка имеется, населена лунатиками";
-    private final String infoAboutThePlant = "Семена подсолнуха. Растут во влажной почве под солнечным светом";
+
     public String getInfoAboutTheEarth() {
-        return this.infoAboutTheEarth;
+        return "Планета Земля: Большая, имеет 6 континентов и" +
+                "4 океана, воздух чистый, состоит преимущественно";
     }
+
     public String getInfoAboutTheMoon() {
-        return this.infoAboutTheMoon;
+        return "Планета Луна: Маленькая, имеет 1 континент" +
+                "1 океан, воздух грязный, состоит преимущественно" +
+                "из сероводорода и азота, твердой оболочка имеется, населена лунатиками";
     }
+
     public String getInfoAboutThePlant() {
-        return this.infoAboutThePlant;
+        return "Семена подсолнуха. Растут во влажной почве под солнечным светом";
     }
 }
