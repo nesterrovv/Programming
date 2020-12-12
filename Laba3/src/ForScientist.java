cc882cd65ecfe30b5a19f2da1a01a38243cf4d2c
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-12 13:47:43 +0700

    Update ForScientist.java

--- Laba3/src/ForScientist.java
+++ Laba3/src/ForScientist.java
@@ -1,13 +1,12 @@
 public interface ForScientist {
     void explorePlanet();
     void explorePlant(Plant p);
-    //void getInformationAboutThePlanet(String dataAboutThePlanet);
-    //void getInformationAboutThePlant(String dataAboutThePlant);
     void plant(Plant p);
     void water(Plant p);
     void setJob(String job);
     String getJob();
     void setName(String name);
     String getName();
-    //boolean hasSeed();
+    boolean equals(String dna);
+    int hashCode(String dna);
 }
