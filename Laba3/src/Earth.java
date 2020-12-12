41fb842dba3940ad8dfad5c7c2d99c7f25cbe5db
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-12 12:03:37 +0700

    Delete Earth.java

--- Laba3/src/Earth.java
+++
@@ -1,5 +0,0 @@
-public class Earth extends Planet {
-    private String name = "Неизвестная планета";
-    private String size = "Неизвестный размер";
-    private String type = "Неизвестный тип";
-}
