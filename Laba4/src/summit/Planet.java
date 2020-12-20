a0e238dd388fe7ee4d181cd5fc0119e824cf2499
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-20 12:59:30 +0700

    Create Planet.java

---
+++ Laba4/src/summit/Planet.java
@@ -0,0 +1,32 @@
+package summit;
+
+public class Planet {
+
+    private String name;
+    private String type;
+    private String size;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSize() {
+        return this.size;
+    }
+}
