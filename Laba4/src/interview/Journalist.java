9387df7c39336f25afe557ea184d6e00402b14f6
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-20 11:20:04 +0700

    Update Journalist.java

--- Laba4/src/interview/Journalist.java
+++ Laba4/src/interview/Journalist.java
@@ -1,4 +1,35 @@
 package interview;
 
-public class Journalist {
+public class Journalist extends HomoSapiens {
+
+    public String interview() {
+        System.out.println("Как вам наша планета? Не желаете ли что-то купить?");
+        System.out.println("Расскажите, пожалуйта, о себе!");
+        System.out.println("Мы давно не видели такого храбреца!");
+        return "Отлично! Как раз успеем к эфиру!";
+    }
+
+    public String scandalousInterview() {
+        System.out.println("Нагло расталкивая окружающих," + getName() + "пробиралась к цели");
+        System.out.println();
+        return "Оно того стоило!";
+    }
+
+    public void photo() {
+        ToPhoto toPhoto = new ToPhoto() {
+            @Override
+            public String takePhoto() {
+                return "Фото готово!";
+            }
+        };
+    }
+
+    public void getAd() {
+        System.out.println("Журналист всучила рекламный плакат");
+        // traveler.hasAd = true
+    }
+    public String leave() {
+        System.out.println("Спасайся кто может!");
+        return "Журналист спешно покинул здание.";
+    }
 }
