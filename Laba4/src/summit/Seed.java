f50aae8c6caa331d838dd12503942fc8bb7eaf51
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-20 12:59:40 +0700

    Create Seed.java

---
+++ Laba4/src/summit/Seed.java
@@ -0,0 +1,12 @@
+package summit;
+
+public class Seed extends Plant {
+    public String isPlant() {
+        return "Семена еще не проросли! Подождите. ";
+    }
+    public String dna() {
+        return "AATCTTATGATATCAATCG";
+    }
+
+    public String getDescription() {return "Арбузные семечки";}
+}
