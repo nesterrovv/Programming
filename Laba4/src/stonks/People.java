5b54f0723a67644d90cd8eb7b69e230bcf431634
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-20 12:58:56 +0700

    Create People.java

---
+++ Laba4/src/stonks/People.java
@@ -0,0 +1,11 @@
+package stonks;
+
+import interview.HomoSapiens;
+
+public class People extends HomoSapiens {
+
+    public void buy() {
+        System.out.println("Бездумные людишки хотят купить хоть что-то " +
+                "ведь ЭТО БЫЛО В РЕКЛАМЕ!");
+    }
+}
