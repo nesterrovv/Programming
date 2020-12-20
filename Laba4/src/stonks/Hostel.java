121e3e7543d4db73c84b7729ede6bd28ef3b27a3
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-20 12:58:54 +0700

    Create Hostel.java

---
+++ Laba4/src/stonks/Hostel.java
@@ -0,0 +1,11 @@
+package stonks;
+
+public class Hostel {
+    private final int visitors = (int) (Math.random()*100); // Randomly amount of visitors
+
+    public void sell() {
+        System.out.println("Реклама Незнайки позволяет гостинице" +
+                " получать гораздо больше денег. Все хотят купить жить только в этой.");
+        System.out.println("Сегодня в гостиницу заселились" + visitors + "посетителей");
+    }
+}
