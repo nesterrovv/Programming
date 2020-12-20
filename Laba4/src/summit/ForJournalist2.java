3be94f09cbc246ffc95b22e0f81d55be22777db6
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-20 12:59:12 +0700

    Create ForJournalist2.java

---
+++ Laba4/src/summit/ForJournalist2.java
@@ -0,0 +1,24 @@
+package summit;
+
+public interface ForJournalist2 {
+
+    //String say(String name);
+    void say(Traveler2 traveler2);
+    void setInformationAboutTheEarth(String inform);
+    String getInformationAboutTheMoon();
+    //boolean hasSeed();
+    void setTheme(String theme);
+    String getTheme();
+    void setOpponents(String opponent);
+    String getOpponent();
+    void setName(String name);
+    String getName();
+    void setHomePlanet(String homePlanet);
+    String getHomePlanet();
+    void setJob(String job);
+    String getJob();
+    String toString(Traveler2 traveler2);
+    //String eat(Food f);
+    //String eat(Drink d);
+    //String health();
+}
