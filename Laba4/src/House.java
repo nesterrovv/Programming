07a64289c2daf9482b01b86b72f732ecef53a9f9
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-22 14:50:44 +0700

    Update House.java

--- Laba4/src/House.java
+++ Laba4/src/House.java
@@ -4,11 +4,16 @@ public class House {
 
     public static class Office {
         private String aim = "ООО Общество гиганстких растений";
+        private int actions = 5000000;
+        private int stock = 0;
         private boolean hasChest = true;
 
-        public void sell(Human human) {
-            human.money(10);
+        public void sell(Human human, int howMuch) {
+            human.money(howMuch);
+            actions -= howMuch;
+            stock += 100*howMuch;
             System.out.println("Сделка завершена. Акции проданы.");
+            System.out.println("Продано " + howMuch + " акций. Прибыль: " + actions);
         }
 
 
