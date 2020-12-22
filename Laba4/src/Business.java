7d00d08966f1221ebce100696d85591e70864a55
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-22 13:14:57 +0700

    Create Business.java

---
+++ Laba4/src/Business.java
@@ -0,0 +1,19 @@
+/**
+ * The ancestor class for all business.
+ */
+
+abstract public class Business {
+
+    private int profitLevel;
+    private boolean popularity;
+
+    public Business(int profitLevel) {
+        this.profitLevel = profitLevel;
+    }
+/*
+    public void isPopular() {
+        this.popularity = true;
+    }
+*/
+
+}
