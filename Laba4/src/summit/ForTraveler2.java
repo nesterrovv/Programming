c42c4c8f585cc6db0025e2e91041acf6fad50750
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-22 13:16:43 +0700

    Delete ForTraveler2.java

--- Laba4/src/summit/ForTraveler2.java
+++
@@ -1,9 +0,0 @@
-package summit;
-public interface ForTraveler2 {
-    void eat(Food f);
-    void drink(Drink d);
-    void possibilityToGiveInformation();
-    boolean returnPossibility();
-    void takeSeed();
-    String getHealthStatus();
-}
