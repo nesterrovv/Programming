ecc71ec71c3c7111410d0cd5a09cb10ec1173b72
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-22 13:13:30 +0700

    Create Flight.java

---
+++ Laba5/src/summit/Flight.java
@@ -0,0 +1,27 @@
+package summit;
+
+public class Flight implements Rocket {
+
+    private Location startPosition;
+    private Location finishPosition;
+    private Location currentPosition;
+
+    public Location where() {
+        return this.currentPosition;
+    }
+
+    public void changeLocation(Location l) {
+        this.currentPosition = l;
+    }
+
+    public void setStartPosition(Location l) { startPosition = l;
+    }
+
+    public void setFinishPosition(Location l) {
+        this.finishPosition = l;
+    }
+
+    public void start() {
+        this.currentPosition = Location.SPACE;
+    }
+}
