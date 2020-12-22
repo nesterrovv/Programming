3d592d35563bc8a82521739aad5fd4df9f5340ca
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-22 13:17:07 +0700

    Delete Rocket.java

--- Laba4/src/summit/Rocket.java
+++
@@ -1,8 +0,0 @@
-package summit;
-
-public interface Rocket {
-    Location where();
-    void changeLocation(Location l);
-    void setStartPosition(Location l);
-    void setFinishPosition(Location l);
-}
