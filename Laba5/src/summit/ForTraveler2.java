10e58b02975352ff76c2c2464bd9ffea4c91b71f
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-22 13:13:42 +0700

    Create ForTraveler2.java

---
+++ Laba5/src/summit/ForTraveler2.java
@@ -0,0 +1,9 @@
+package summit;
+public interface ForTraveler2 {
+    void eat(Food f);
+    void drink(Drink d);
+    void possibilityToGiveInformation();
+    boolean returnPossibility();
+    void takeSeed();
+    String getHealthStatus();
+}
