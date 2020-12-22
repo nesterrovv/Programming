620c80dfdad0427473a6a2fb9e97574ffa64d3b5
Ivan Nesterov <nesterov.iv2018@gmail.com>
2020-12-22 13:15:30 +0700

    Delete Police.java

--- Laba4/src/interview/Police.java
+++
@@ -1,9 +0,0 @@
-package interview;
-
-public class Police extends HomoSapiens {
-    public String cancelMeeting() {
-        System.out.println("Электрические дубинки и грубая сила - эффективный способ" +
-                           " утихомирить буйную толпу");
-        return "Бунт подавлен.";
-    }
-}
