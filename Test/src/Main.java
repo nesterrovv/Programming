fdfda0443039b287db179be94d33867c36d76ca4
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-02-25 20:00:54 +0700

    Create Main.java

---
+++ Test/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        String x = "йййй";
+        x = "фцыувкаепнр";
+        System.out.print(x);
+    }
+}
