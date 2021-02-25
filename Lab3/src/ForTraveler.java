ab7419a72d26c0c9ed4cc24786d9f314c3a0d2b1
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-02-25 19:32:47 +0700

    Create ForTraveler.java

---
+++ Lab3/src/ForTraveler.java
@@ -0,0 +1,8 @@
+public interface ForTraveler {
+    void eat(Food f);
+    void drink(Drink d);
+    void possibilityToGiveInformation();
+    boolean returnPossibility();
+    void takeSeed();
+    String getHealthStatus();
+}
