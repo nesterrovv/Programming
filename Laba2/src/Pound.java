d3a9e802f84bde68299c4fe1318ae130d19239c9
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-02-25 19:46:06 +0700

    Delete Pound.java

--- Laba2/src/Pound.java
+++
@@ -1,9 +0,0 @@
-import ru.ifmo.se.pokemon.*;
-/*
-Pound deals damage with no additional effect.
- */
-class Pound extends PhysicalMove {
-    protected Pound() {
-        super(Type.NORMAL, 40.0, 100.0);
-    }
-}
