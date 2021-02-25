603d41928043ebcfe4e79d4699ecdde7a6353bdc
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-02-25 19:30:31 +0700

    Create Pound.java

---
+++ Lab2/src/Pound.java
@@ -0,0 +1,9 @@
+import ru.ifmo.se.pokemon.*;
+/*
+Pound deals damage with no additional effect.
+ */
+class Pound extends PhysicalMove {
+    protected Pound() {
+        super(Type.NORMAL, 40.0, 100.0);
+    }
+}
