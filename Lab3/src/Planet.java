5a7453d2963a142537d3f9dbfc7523918e18abdd
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-02-25 19:35:12 +0700

    Create Planet.java

---
+++ Lab3/src/Planet.java
@@ -0,0 +1,30 @@
+public class Planet {
+
+    private String name;
+    private String type;
+    private String size;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSize() {
+        return this.size;
+    }
+}
