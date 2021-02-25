1f68c08f4645263a6ba290350632e5e796ed8a52
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-02-25 19:32:15 +0700

    Create ForJournalist.java

---
+++ Lab3/src/ForJournalist.java
@@ -0,0 +1,22 @@
+public interface ForJournalist {
+
+    //String say(String name);
+    void say(Traveler traveler);
+    void setInformationAboutTheEarth(String inform);
+    String getInformationAboutTheMoon();
+    //boolean hasSeed();
+    void setTheme(String theme);
+    String getTheme();
+    void setOpponents(String opponent);
+    String getOpponent();
+    void setName(String name);
+    String getName();
+    void setHomePlanet(String homePlanet);
+    String getHomePlanet();
+    void setJob(String job);
+    String getJob();
+    String toString(Traveler traveler);
+    //String eat(Food f);
+    //String eat(Drink d);
+    //String health();
+}
