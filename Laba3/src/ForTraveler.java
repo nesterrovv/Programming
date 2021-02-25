41b6671e52d14d161700efb672b79a6ed3dbe70b
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-02-25 19:53:40 +0700

    Delete ForTraveler.java

--- Laba3/src/ForTraveler.java
+++
@@ -1,8 +0,0 @@
-public interface ForTraveler {
-    void eat(Food f);
-    void drink(Drink d);
-    void possibilityToGiveInformation();
-    boolean returnPossibility();
-    void takeSeed();
-    String getHealthStatus();
-}
