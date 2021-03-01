0c86f97e83aa2f75c1f67f9b8c4a5128e7827026
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-01 17:59:42 +0700

    Create Employees.java

---
+++ Parser/src/Employees.java
@@ -0,0 +1,29 @@
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "employees")
+@XmlAccessorType (XmlAccessType.FIELD)
+public class Employees
+{
+    @XmlElement(name = "employee")
+    private List<Employee> employees = null;
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Employees{" +
+                "employees=" + employees +
+                '}';
+    }
+}
