30bdebe11eb742c32407fa797dec05c907561b4c
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-03 00:43:03 +0700

    Update Employees.java

--- Test/src/Employees.java
+++ Test/src/Employees.java
@@ -3,6 +3,18 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.*;
+import javax.xml.bind.*;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 
 @XmlRootElement(name = "employees")
 @XmlAccessorType (XmlAccessType.FIELD)
@@ -24,5 +36,38 @@ public class Employees
         return "Employees{" +
                 "employees=" + employees +
                 '}';
+    }
+
+    public void marshalElement(int id, String firstName, String lastName, double income) {
+        try {
+
+            // making new element
+            Employee newEmployee = new Employee();
+            newEmployee.setId(4);
+            newEmployee.setFirstName("Ivan");
+            newEmployee.setLastName("Nesterov");
+            newEmployee.setIncome(1000.0);
+            employees.add(newEmployee);
+
+            // marshaling an element
+            System.out.println("Ok");
+            JAXBContext jaxbContext = JAXBContext.newInstance(Employees.class);
+            System.out.println("Ok");
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            System.out.println("Ok");
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            System.out.println("Ok");
+            employees.add(newEmployee);
+            System.out.println("Ok");
+            System.out.println(employees);
+            //Marshal the employees list in console
+            jaxbMarshaller.marshal(employees, System.out);
+            System.out.println("kek");
+            //Marshal the employees list in file
+            jaxbMarshaller.marshal(employees, new File("employees.xml"));
+            System.out.println("Ok");
+        } catch (JAXBException jaxbException) {
+            System.out.println("Хуевый маршалинг");
+        }
     }
 }
