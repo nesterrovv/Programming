9b86bd250cc9ad3862484a77064f4802de611753
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-19 23:31:14 +0300

    Update Person.java

--- Lab5/src/data/Person.java
+++ Lab5/src/data/Person.java
@@ -1,28 +1,51 @@
 package data;
 
-import java.time.ZonedDateTime;
+import javax.print.DocFlavor;
+import javax.xml.bind.annotation.*;
+import java.time.LocalDate;
+import java.time.*;
 
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "employee")
+@XmlAccessorType(XmlAccessType.NONE)
 public class Person {
+    @XmlElement
     private int id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    @XmlElement
     private String name; //Поле не может быть null, Строка не может быть пустой
+    @XmlElement
     private Coordinates coordinates; //Поле не может быть null
-    private java.time.ZonedDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    @XmlElement
+    private String creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    @XmlElement
     private long height; //Значение поля должно быть больше 0
+    @XmlElement
     private EyeColor eyeColor; //Поле не может быть null
+    @XmlElement
     private HairColor hairColor; //Поле может быть null
+    @XmlElement
     private Country nationality; //Поле не может быть null
+    @XmlElement
     private Location location; //Поле не может быть null
 
-    public Person(String name, Coordinates coordinates, ZonedDateTime creationDate, long height, EyeColor eyeColor, HairColor hairColor, Country nationality, Location location) {
+
+    public Person(int id, String name, Coordinates coordinates, String creationDate, long height, EyeColor eyeColor, HairColor hairColor, Country nationality, Location location) {
+        this.id = id;
         this.name = name;
         this.coordinates = coordinates;
-        this.creationDate = creationDate;
+        this.creationDate = returnCreationDate();
         this.height = height;
         this.eyeColor = eyeColor;
         this.hairColor = hairColor;
         this.nationality = nationality;
         this.location = location;
     }
+
+    public Person() {}
 
     @Override
     public String toString() {
@@ -39,7 +62,73 @@ public class Person {
                 '}';
     }
 
+
+
     public int getId() {
         return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public long getHeight() {
+        return height;
+    }
+
+    public void setHeight(long height) {
+        this.height = height;
+    }
+
+    public EyeColor getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(EyeColor eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public HairColor getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(HairColor hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public Country getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(Country nationality) {
+        this.nationality = nationality;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String returnCreationDate() {
+        return ZonedDateTime.now().toString();
     }
 }
