7e4abffc3db0f81857b7db637617226801824629
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-19 23:31:20 +0300

    Update Main.java

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -1,14 +1,9 @@
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-import data.*;
-import java.time.ZonedDateTime;
-import java.util.*;
 import java.io.*;
-import javax.xml.bind.JAXBException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        CollectionManager collectionManager = new CollectionManager();
-            collectionManager.add();
+    public static void main(String[] args) throws IOException {
+        Commander commander = new Commander(new CollectionManager());
+        commander.interactiveMod();
     }
 }
