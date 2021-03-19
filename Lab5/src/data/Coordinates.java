f739ee7cc4caaf3300b841e83bc423ddd00ef72e
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-19 23:31:06 +0300

    Update Coordinates.java

--- Lab5/src/data/Coordinates.java
+++ Lab5/src/data/Coordinates.java
@@ -1,12 +1,28 @@
 package data;
 
+import javax.xml.bind.annotation.*;
+
+
+@XmlType(name = "coordinates")
+@XmlRootElement
 public class Coordinates {
+    @XmlElement
     private long x; //Максимальное значение поля: 690
+    @XmlElement
     private Float y; //Поле не может быть null
 
     public Coordinates(long x, Float y) {
-        this.x = (long) x;
-        this.y = new Float(y);
+        this.x = x;
+        this.y = y;
     }
 
+    public Coordinates() {}
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
