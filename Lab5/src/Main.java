378050118b06b4494e4bb2e56d0088ce70f378c8
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-20 23:43:56 +0300

    Update Main.java

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -1,19 +1,8 @@
-import data.Country;
-
 import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        /*
-        CollectionManager collectionManager = new CollectionManager();
-        Country c1 = collectionManager.receiveNationality();
-        Country c2 = collectionManager.receiveNationality();
-        Country c3 = collectionManager.receiveNationality();
-        System.out.println(c1.hashCode());
-        System.out.println(c2.hashCode());
-        System.out.println(c3.hashCode());
-         */
+        System.out.println("Collection manager v1.0 by Ivan Nesterov is starting!");
         Commander commander = new Commander(new CollectionManager());
         commander.interactiveMod();
     }
