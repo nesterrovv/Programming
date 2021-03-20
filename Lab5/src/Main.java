4a6063847bd502657d01b9079b780e0969f16d20
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-20 05:30:44 +0300

    Update Main.java

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -1,8 +1,19 @@
+import data.Country;
+
 import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        /*
+        CollectionManager collectionManager = new CollectionManager();
+        Country c1 = collectionManager.receiveNationality();
+        Country c2 = collectionManager.receiveNationality();
+        Country c3 = collectionManager.receiveNationality();
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c3.hashCode());
+         */
         Commander commander = new Commander(new CollectionManager());
         commander.interactiveMod();
     }
