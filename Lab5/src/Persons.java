5abcaf6c000dee0a35d989fe1479ac0b2a6fc8ff
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-20 23:44:01 +0300

    Update Persons.java

--- Lab5/src/Persons.java
+++ Lab5/src/Persons.java
@@ -1,15 +1,11 @@
 import data.Person;
-import org.omg.CORBA.PERSIST_STORE;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.List;
 
 @XmlRootElement(name = "persons")
 @XmlAccessorType (XmlAccessType.NONE)
