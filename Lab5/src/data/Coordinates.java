d12a04ba70be8f9a885b31d36b98305cfe8d8cf4
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-21 16:56:32 +0300

    Update Coordinates.java

--- Lab5/src/data/Coordinates.java
+++ Lab5/src/data/Coordinates.java
@@ -2,22 +2,35 @@ package data;
 
 import javax.xml.bind.annotation.*;
 
-
+/**
+ * @author Ivan Nesterov
+ * @version 1.0
+ * Сlass for describing field Coordinates of element
+ */
 @XmlType(name = "coordinates")
 @XmlRootElement
 public class Coordinates {
+    /** field x */
     @XmlElement
     private long x; //Максимальное значение поля: 690
+    /** field x */
     @XmlElement
     private Float y; //Поле не может быть null
 
+    /**
+     * Constructor for making Coordinates field
+     * @param x - x-coordinate
+     * @param y - y-coordinate
+     */
     public Coordinates(long x, Float y) {
         this.x = x;
         this.y = y;
     }
 
+    /** Default constructor */
     public Coordinates() {}
 
+    /** Method for printing this field into a string representation */
     @Override
     public String toString() {
         return "Coordinates{" +
