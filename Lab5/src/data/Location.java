c59aaaa8ca9b46f29d2ad2f1f9a22e69bdfaf6a5
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-21 16:56:54 +0300

    Update Location.java

--- Lab5/src/data/Location.java
+++ Lab5/src/data/Location.java
@@ -4,24 +4,42 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
+/**
+ * @author Ivan Nesterov
+ * @version 1.0
+ * Class for describing a field Location of element
+ */
 @XmlType(name = "location")
 @XmlRootElement
 public class Location {
+    /** field x */
     @XmlElement
     private long x;
+    /** field y */
     @XmlElement
     private Double y; //Поле не может быть null
+    /** field name */
     @XmlElement
     private String name; //Поле не может быть null
 
+    /**
+     * Constructor
+     * @param x - x-coordinate
+     * @param y - y-coordinate
+     * @param name - name of location
+     */
     public Location(long x, Double y, String name) {
         this.x = x;
         this.y = y;
         this.name = name;
     }
 
+    /**
+     * Default constructor
+     */
     public Location() {}
 
+    /** Method for printing this field into a string representation */
     @Override
     public String toString() {
         return "Location{" +
