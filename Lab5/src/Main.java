5464572a41676c16b05483da8f3350e9be43d110
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-03-21 16:57:01 +0300

    Update Main.java

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -1,6 +1,13 @@
 import java.io.*;
 
 public class Main {
+    /**
+     * Main class for starting a program
+     * @author Ivan Nesterov
+     * @version 1.0
+     * @param args - args for program successfully working
+     * @throws IOException - may be throw
+     */
     public static void main(String[] args) throws IOException {
         System.out.println("Collection manager v1.0 by Ivan Nesterov is starting!");
         Commander commander = new Commander(new CollectionManager());
