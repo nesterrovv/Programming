18917dc55ff76c1469d3aa91c273393b9150072b
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-22 02:50:32 +0300

    Update AddCommand.java

--- Lab6/src/server/commands/AddCommand.java
+++ Lab6/src/server/commands/AddCommand.java
@@ -1,4 +1,17 @@
 package server.commands;
 
-public class AddCommand {
+import server.serverCode.CollectionManager;
+
+public class AddCommand extends AbstractCommand {
+
+    public AddCommand(CollectionManager manager) {
+        super(manager);
+        setDescription("Adds new element to the collection.");
+    }
+
+    @Override
+    public synchronized String execute() {
+        getManager().add();
+        return "Element is added.";
+    }
 }
