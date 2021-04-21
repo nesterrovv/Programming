f810b12e8b02903e6663df0017f028efd51cf22e
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-22 02:51:34 +0300

    Create SaveCommand.java

---
+++ Lab6/src/server/commands/SaveCommand.java
@@ -0,0 +1,16 @@
+package server.commands;
+
+import server.serverCode.CollectionManager;
+
+public class SaveCommand extends AbstractCommand {
+
+    public SaveCommand(CollectionManager manager) {
+        super(manager);
+        setDescription("Does nothing. Saving is an automatic process :)");
+    }
+
+    @Override
+    public synchronized String execute() {
+        return "Don`t worry, saving is an automatic process starting after some changes in collection :)";
+    }
+}
