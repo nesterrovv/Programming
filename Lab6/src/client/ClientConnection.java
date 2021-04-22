eec5adecfb3a8df29dc55d8c26b7ebfde2a92333
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-22 04:34:34 +0300

    Update ClientConnection.java

--- Lab6/src/client/ClientConnection.java
+++ Lab6/src/client/ClientConnection.java
@@ -1,128 +1,116 @@
-import data.Person;
+package client;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
-import java.util.NoSuchElementException;
+import java.util.Scanner;
 
-public class Client {
+/**
+ * Класс {@code ClientConnection} представляет объект клиента, подключаемый к серверу для манипулирования коллекцией.
+ */
+public class ClientConnection {
 
-    private static Socket clientSocket;
-    private static BufferedReader reader;
-    private static BufferedReader in;
-    private static BufferedWriter out;
+    private static Scanner fromKeyboard;
+    private static ObjectOutputStream toServer;
+    private static ObjectInputStream fromServer;
 
-    /** Collection manager for realising user`s commands */
-    private final CollectionManager collectionManager;
-    /** Field for receiving user`s command */
-    private String userCommand;
-    /** Field for separating user input into a command and an argument to it */
-    private static String[] finalUserCommand;
-
-    {
-        userCommand = "";
-    }
-
-    public Client(CollectionManager manager) {
-        this.collectionManager = manager;
-        try {
-            clientSocket = new Socket("localhost", 4242);
-            reader = new BufferedReader(new InputStreamReader(System.in));
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        } catch (IOException ioException) {
-            System.out.println("Server is not available. Reconnection...");
-        }
-    }
-
-    public void interactiveMod() {
-        try {
-            try {
-                while (!userCommand.equals("exit")) {
-                    System.out.print("Enter a command: ");
-                    userCommand = reader.readLine();
-                    finalUserCommand = userCommand.trim().toLowerCase().split(" ", 2);
-                    try {
-                        switch (finalUserCommand[0]) {
+    /**
+     * Устанавливает активное соединение с сервером.
+     */
+    public void work() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            fromKeyboard = scanner;
+            while (true) {
+                try (Socket outcoming = new Socket(InetAddress.getLocalHost(), 8800)) {
+                    outcoming.setSoTimeout(5000);
+                    try (ObjectOutputStream outputStream = new ObjectOutputStream(outcoming.getOutputStream());
+                         ObjectInputStream inputStream = new ObjectInputStream(outcoming.getInputStream())) {
+                        toServer = outputStream;
+                        fromServer = inputStream;
+                        interactiveMode();
+                        System.out.println("Завершение программы.");
+                    }
+                } catch (IOException e) {
+                    System.err.println("Нет связи с сервером. Подключться ещё раз (введите {да} или {нет})?");
+                    String answer;
+                    while (!(answer = fromKeyboard.nextLine()).equals("да")) {
+                        switch (answer) {
                             case "":
                                 break;
-                            case "help":
-                                out.write("help" + "\n");
-                                out.flush();
+                            case "нет":
+                                exit();
                                 break;
-                            case "info":
-                                out.write("info" + "\n");
-                                out.flush();
-                                break;
-                            case "show":
-                                out.write("show" + "\n");
-                                out.flush();
-                                break;
-                            case "add":
-                                out.write("add" + "\n");
-                                out.flush();
-                                break;
-                            case "update_by_id":
-                                out.write("update_by_id" + finalUserCommand[1].toString() + "\n");
-                                out.flush();
-                                break;
-                            case "remove_by_id":
-                                out.write("remove_by_id" + finalUserCommand[1].toString() + "\n");
-                                out.flush();
-                                break;
-                            case "clear":
-                                out.write("clear" + "\n");
-                                out.flush();
-                                break;
-                            case "save":
-                                System.out.println("Saving is an automatic process.");
-                                break;
-                            case "execute_script":
-                                collectionManager.execute_script(finalUserCommand[1]);
-                                break;
-                            case "exit":
-                                collectionManager.exit();
-                                break;
-                            case "add_if_min":
-                                System.out.println("Enter characteristics of element, which will be compared with elements in collection.");
-                                collectionManager.add_if_min(new Person(collectionManager.receiveId(), collectionManager.receiveName(),
-                                        collectionManager.receiveCoordinates(), collectionManager.returnDate(),
-                                        collectionManager.receiveHeight(), collectionManager.receiveEyeColor(),
-                                        collectionManager.receiveHairColor(), collectionManager.receiveNationality(),
-                                        collectionManager.receiveLocation()));
-                                break;
-                            case "remove_greater":
-                                System.out.println("Enter characteristics of element, which will be compared with elements in collection.");
-                                collectionManager.remove_greater(collectionManager.receiveHeight());
-                                break;
-                            case "remove_lower":
-                                System.out.println("Enter characteristics of element, which will be compared with elements in collection.");
-                                collectionManager.remove_lower(collectionManager.receiveHeight());
-                                break;
-                            case "sum_of_height":
-                                collectionManager.sum_of_height();
-                                break;
-                            case "group_counting_by_nationality":
-                                collectionManager.group_counting_by_nationality();
-                                break;
-                            case "count_greater_than_nationality":
-                                System.out.println("Enter nationality, which will be compared with element`s nationality.");
-                                collectionManager.count_greater_than_nationality(collectionManager.receiveNationality());
-                                break;
-                            default:
-                                System.out.println("Unknown command. Write help for help.");
-                                break;
+                            default: System.out.println("Введите корректный ответ.");
                         }
-                    } catch (ArrayIndexOutOfBoundsException ex) {
-                        System.out.println("Argument of command is absent. Write help for help.");
                     }
+                    System.out.print("Подключение ...");
                 }
-            } catch (IOException ioException) {
-                System.out.println("Input output exception");
             }
-        } catch (NoSuchElementException noSuchElementException) {
-            System.out.println("Program will be finished now.");
-            System.exit(1);
         }
     }
 
+    /**
+     * Парсит пользовательские команды и осуществляет обмен данными с сервером.
+     * @throws IOException при отправке и получении данных с сервера.
+     */
+    private void interactiveMode() throws IOException {
+        try {
+            System.out.println((String) fromServer.readObject());
+            String command;
+            while (!(command = fromKeyboard.nextLine()).equals("exit")) {
+                String[] parsedCommand = command.trim().split(" ", 2);
+                switch (parsedCommand[0]) {
+                    case "":
+                        break;
+                    case "import":
+                        try {
+                            toServer.writeObject(importingFile(parsedCommand[1]));
+                            System.out.println((String) fromServer.readObject());
+                        } catch (FileNotFoundException e) {
+                            System.out.println("Файл по указанному пути не найден.");
+                        } catch (SecurityException e) {
+                            System.out.println("Файл защищён от чтения.");
+                        } catch (IOException e) {
+                            System.out.println("Что-то не так с файлом.");
+                        }
+                        break;
+                    default:
+                        toServer.writeObject(command);
+                        System.out.println((String) fromServer.readObject());
+                }
+            }
+            exit();
+        } catch (ClassNotFoundException e) {
+            System.err.println("Класс не найден: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Импортирует локальный файл и отправляет на сервер.
+     * @param path путь к файлу.
+     * @return команду для сервера и содержимое файла.
+     * @throws SecurityException если отсутствуют права rw.
+     * @throws IOException если файл не существует.
+     */
+    private String importingFile(String path) throws SecurityException, IOException {
+        File localCollection = new File(path);
+        String extension = localCollection.getAbsolutePath().substring(localCollection.getAbsolutePath().lastIndexOf(".") + 1);
+        if (!localCollection.exists() | localCollection.length() == 0  | !extension.equals("json"))
+            throw new FileNotFoundException();
+        if (!localCollection.canRead()) throw new SecurityException();
+        try (BufferedReader inputStreamReader = new BufferedReader(new FileReader(localCollection))) {
+            String nextLine;
+            StringBuilder result = new StringBuilder();
+            while ((nextLine = inputStreamReader.readLine()) != null) result.append(nextLine);
+            return "import " + result.toString();
+        }
+    }
+
+    /*
+    Отвечает за завершение работу клиентского приложения.
+     */
+    private void exit() {
+        System.out.println("Завершение программы.");
+        System.exit(0);
+    }
 }
