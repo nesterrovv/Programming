5d32c552e7ae54cb6d15d23558d9d4000d1312b3
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-22 10:58:14 +0300

    Update ClientSide.java

--- Lab6/src/client/ClientSide.java
+++ Lab6/src/client/ClientSide.java
@@ -2,10 +2,6 @@ package client;
 
 public class ClientSide {
 
-    /**
-     * Отправная точка клиента. Создает объект {@code ClientConnection}.
-     * @param args массив по умолчанию в основном методе. Не используется здесь.
-     */
     public static void main(String[] args) {
         System.out.println("Запуск клиентского модуля.\nПодключение к серверу ...");
         ClientConnection connection = new ClientConnection();
