f28f97cd31305b020be663b90cb22ce9ac5753d0
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-22 20:49:03 +0300

    Create ServerSide.java

---
+++ Server/src/server/serverCode/ServerSide.java
@@ -0,0 +1,42 @@
+package server.serverCode;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerSide {
+
+    private static final CollectionManager serverCollection = new CollectionManager();
+
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(8800)) {
+            System.out.print("Server is accepting a client. " + "\nPort " + server.getLocalPort() +
+                    " / Address " + InetAddress.getLocalHost() + ".\nWaiting a connection of clients... ");
+            Thread pointer = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    System.out.print(".");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        System.out.print("\n");
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            });
+            pointer.setDaemon(true);
+            pointer.start();
+            while (true) {
+                Socket incoming = server.accept();
+                pointer.interrupt();
+                System.out.println(incoming + " is connected to server.");
+                Runnable r = new ServerConnection(serverCollection, incoming);
+                Thread t = new Thread(r);
+                t.start();
+            }
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        }
+    }
+}
