d8a1935d3104b59c68ea6a596fd82c66aeec2f0e
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-29 16:24:15 +0300

    Create Country.java

---
+++ Test/src/data/Country.java
@@ -0,0 +1,12 @@
+package data;
+
+/**
+ * @author Ivan Nesterov
+ * @version 1.0
+ * Enum class for describing nationality of element
+ */
+public enum Country {
+    GERMANY,
+    CHINA,
+    NORTH_KOREA
+}
