71a03970f35121da7d22ee3732b1afef32a3a6d0
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-29 16:18:53 +0300

    Update UpdateByIdCommand.java

--- Lab6/src/server/commands/UpdateByIdCommand.java
+++ Lab6/src/server/commands/UpdateByIdCommand.java
@@ -1,6 +1,11 @@
 package server.commands;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import server.data.Person;
 import server.serverCode.CollectionManager;
+
+import java.io.IOException;
+import java.util.HashSet;
 
 public class UpdateByIdCommand extends AbstractCommand {
 
@@ -9,9 +14,23 @@ public class UpdateByIdCommand extends AbstractCommand {
         setDescription("Updates an element finding it by it`s ID.");
     }
 
-    @Override
-    public synchronized String execute(String id) {
-        getManager().update_by_id(id);
-        return "Command is completed.";
+    //@Override
+    public String execute (String id, String arg) {
+        HashSet<Person> persons = getManager().getPersons();
+        for (Person person : persons) {
+            int intId = person.getId();
+            String strId = String.valueOf(intId);
+            if (strId.equals(id)) {
+                persons.remove(person);
+                XmlMapper mapper = new XmlMapper();
+                try {
+                    getManager().getPersons().add(mapper.readValue(arg, Person.class));
+                    return "Element was added successfully.";
+                } catch (IOException ioException) {
+                    return "Something bad with updating element.";
+                }
+            }
+        }
+        return null;
     }
 }
