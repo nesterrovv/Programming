7011bc61e729540fc4c2081f572f8b21830394de
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-29 16:18:40 +0300

    Update RemoveLowerCommand.java

--- Lab6/src/server/commands/RemoveLowerCommand.java
+++ Lab6/src/server/commands/RemoveLowerCommand.java
@@ -2,6 +2,7 @@ package server.commands;
 
 import server.data.Person;
 import server.serverCode.CollectionManager;
+import java.util.HashSet;
 
 public class RemoveLowerCommand extends AbstractCommand {
 
@@ -10,8 +11,17 @@ public class RemoveLowerCommand extends AbstractCommand {
         setDescription("Removes all elements which height less than current.");
     }
 
-    public synchronized String execute(long height) {
-        getManager().remove_lower(height);
-        return "Command is completed.";
+    public synchronized String execute(String arg) {
+        Long longHeight = new Long(Long.valueOf(arg));
+        long height = longHeight;
+        HashSet<Person> persons = getManager().getPersons();
+        int counter = 0;
+        for (Person person : persons) {
+            if (person.getHeight() > height) {
+                persons.remove(person);
+                counter += 1;
+            }
+        }
+        return "Operation was finished successfully. " + counter + " elements were deleted.";
     }
 }
