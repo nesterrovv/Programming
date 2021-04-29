f80fd39e8ec9df0b83b8c1cac99eadc1463331e3
Ivan Nesterov <nesterov.iv2018@gmail.com>
2021-04-29 16:21:22 +0300

    Update RemoveByIdCommand.java

--- Server/src/server/commands/RemoveByIdCommand.java
+++ Server/src/server/commands/RemoveByIdCommand.java
@@ -1,6 +1,11 @@
 package server.commands;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import server.data.Person;
 import server.serverCode.CollectionManager;
+
+import java.io.IOException;
+import java.util.HashSet;
 
 public class RemoveByIdCommand extends AbstractCommand {
 
@@ -9,9 +14,17 @@ public class RemoveByIdCommand extends AbstractCommand {
         setDescription("Removes an element finding it by it`s ID.");
     }
 
-    @Override
-    public synchronized String execute(String id) {
-        getManager().remove_by_id(id);
-        return "Command is completed.";
+    //@Override
+    public String execute (String id, String arg) {
+        HashSet<Person> persons = getManager().getPersons();
+        for (Person person : persons) {
+            int intId = person.getId();
+            String strId = String.valueOf(intId);
+            if (strId.equals(id)) {
+                persons.remove(person);
+                return "Element was removed successfully.";
+            }
+        }
+        return "Element with this ID is not exist.";
     }
 }
