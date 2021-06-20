431c36a5d70c7881894e26f262d3704608753503
Ivan Nesterov <71551187+nesterrovv@users.noreply.github.com>
2021-06-20 14:01:26 +0300

    Delete Lab7 directory

--- Lab7/Client/src/ClientSide.java
+++
@@ -1,8 +0,0 @@
-public class ClientSide {
-
-    public static void main(String[] args) {
-        System.out.println("Starting a client moodle.\nConnecting to server...");
-        ClientConnection connection = new ClientConnection();
-        connection.work();
-    }
-}
