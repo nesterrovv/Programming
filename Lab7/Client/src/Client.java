425d930d805d4571e44f9e3aab145d0daf408c99
Ivan Nesterov <71551187+nesterrovv@users.noreply.github.com>
2021-06-21 12:29:40 +0300

    Delete Lab7 directory

--- Lab7/Client/src/Client.java
+++
@@ -1,9 +0,0 @@
-public class Client {
-
-    public static void main(String[] args) {
-        ClientConnection clientConnection = new ClientConnection();
-        clientConnection.connect();
-        //clientConnection.work();
-
-    }
-}
